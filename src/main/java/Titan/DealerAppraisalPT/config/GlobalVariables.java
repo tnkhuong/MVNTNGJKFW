9aebc32875fcc448c2613db0f9619e233ea66a03
Khuong.Truong <truongnguyen.khuong@gmail.com>
2020-03-21 01:22:11 +0700

    Create resources/browserdriver folder

--- src/main/java/Titan/DealerAppraisalPT/config/GlobalVariables.java
+++ src/main/java/Titan/DealerAppraisalPT/config/GlobalVariables.java
@@ -1,9 +1,9 @@
 package Titan.DealerAppraisalPT.config;
 
 public class GlobalVariables {
-	public static String glb_IE_DriverPath = ".\\IEDriverServer.exe";
-	public static String glb_CHR_DriverPath = ".\\chromedriver.exe";
-	public static String glb_FF_DriverPath = ".\\geckodriver.exe";	
+	public static String glb_IE_DriverPath = ".\\src\\resources\\browserdriver\\IEDriverServer.exe";
+	public static String glb_CHR_DriverPath = ".\\src\\resources\\browserdriver\\chromedriver.exe";
+	public static String glb_FF_DriverPath = ".\\src\\resources\\browserdriver\\geckodriver.exe";	
 	public static int DEFAULT_WAIT_4_PAGE = 50;
 	public static int DEFAULT_WAIT_4_CONTROL = 20;
 	public static String glb_Browser="chrome";
